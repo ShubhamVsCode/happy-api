ec0ea4052935d08a18b809b755a2640b84b72ad0
Shubham <shubhamvscode@gmail.com>
2024-02-22 14:40:09 +0530

    refactor: header component seperate

---
+++ src/components/header.tsx
@@ -0,0 +1,55 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
+      <Button className="lg:hidden" variant="ghost">
+        <MenuIcon className="h-6 w-6" />
+        <span className="sr-only">Toggle sidebar</span>
+      </Button>
+      <div className="w-full flex-1">
+        <form>
+          <div className="relative">
+            {/* <SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 dark:text-gray-400" /> */}
+            <Input
+              className="w-full bg-white shadow-none appearance-none pl-8 md:w-2/3 lg:w-1/3 dark:bg-gray-950"
+              placeholder="Search endpoints..."
+              type="search"
+            />
+          </div>
+        </form>
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            className="rounded-full border border-gray-200 w-8 h-8 dark:border-gray-800"
+            size="icon"
+            variant="ghost"
+          >
+            S<span className="sr-only">Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
+  );
+};
+
+export default Header;
